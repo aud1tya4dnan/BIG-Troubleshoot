31945844d5dd40f42711beaf39fdac98baeb35cb
andreasTimo <56831859+andreasTimo@users.noreply.github.com>
2025-06-16 21:52:34 +0700

    Update sysbench_io_test.sh

--- SYSBENCH-script/sysbench_io_test.sh
+++ SYSBENCH-script/sysbench_io_test.sh
@@ -9,7 +9,7 @@ RESULT_DIR="/home/sysbench_tests/io_results_${VM_COUNT}_${INSTANCE_NAME}"
 mkdir -p "$RESULT_DIR"
 
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
-CSV_FILE="$RESULT_DIR/sysbench_io_fileio_palapa_${TIMESTAMP}_${VM_COUNT}_${INSTANCE_NAME}.csv"
+CSV_FILE="$RESULT_DIR/fileio_palapa_${TIMESTAMP}_${VM_COUNT}_${INSTANCE_NAME}.csv"
 
 # Workload / concurrency
 TEST_TIME=60
